545e3f9a544118eda45e313372aba1539fd6d4ba
Andrew Pinkham <code@andrewsforge.com>
2013-09-21 17:16:21 -0400

    Created Views.

--- bank/views.py
+++ bank/views.py
@@ -1,1 +1,40 @@
-# Create your views here.
+# bank/views.py
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Account
+from .forms import AccountForm, TransactionForm
+
+def account_list(request):
+    acct_list = Account.objects.all()
+    return render(request,
+                  'bank/account_list.html',
+                  {'account_list': acct_list})
+
+def account_detail(request, slug):
+    acct = get_object_or_404(Account, slug=slug)
+    return render(request,
+                  'bank/account_detail.html',
+                  {'account': acct})
+
+def account_create(request):
+    if request.method == 'POST':
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            new_acct = form.save()
+            return redirect(new_acct)
+    else:
+        form = AccountForm()
+    return render(request,
+                  'bank/account_form.html',
+                  {'form': form})
+
+def transaction_create(request):
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            new_trans = form.save()
+            return redirect('bank_account_list')
+    else:
+        form = TransactionForm()
+    return render(request,
+                  'bank/transaction_form.html',
+                  {'form': form})
