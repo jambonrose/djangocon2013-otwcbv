c74f5677b610f2f90f477e0d48309a664fc36298
Andrew Pinkham <code@andrewsforge.com>
2013-09-24 12:00:38 -0400

    Removed views - all logic in urls.py

--- bank/urls.py
+++ bank/urls.py
@@ -1,20 +1,21 @@
 # bank/urls.py
 from django.conf.urls import patterns, url
-from .models import Account
-from .views import (AccountList, AccountDetail,
-                   AccountCreate, TransactionCreate)
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Account, Transaction
 
 urlpatterns = patterns('',
     url(r'^account/$',
-        AccountList.as_view(),
+        ListView.as_view(model=Account),
         name='bank_account_list'),
     url(r'^account/create/$',
-        AccountCreate.as_view(),
+        CreateView.as_view(model=Account),
         name='bank_account_create'),
     url(r'^account/(?P<slug>[\w\-]+)/$',
-        AccountDetail.as_view(),
+        DetailView.as_view(model=Account),
         name='bank_account_detail'),
     url(r'^transaction/$',
-        TransactionCreate.as_view(),
+        CreateView.as_view(model=Transaction,
+                           success_url=reverse_lazy('bank_account_list')),
         name='bank_trans_create'),
 )
